939962819da1443454e90eacc36c95cf07cdcdaf
Pablo0304 <gonca.pablo@gmail.com>
2023-07-26 00:31:23 +0200

    Handlebars

--- Section_7(Webserver-Express)/app.js
+++ Section_7(Webserver-Express)/app.js
@@ -1,31 +1,51 @@
 const express = require('express');
 const app = express();
+const hbs = require('hbs');
 
 const port = 8080;
+
+// Handlebars
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials')
 
 // Servir contenido estático('/'):   <= Tiene prioridad, luego las "app.get(...)"
 app.use(express.static('public'));
 
-app.get('/yep', (req, res) => {   // Si el path es "/yep"
+/*app.get('/yep', (req, res) => {   // Si el path es "/yep"
     res.send('Hola desde "/yep"!');
-});
-
-app.get('/', (req, res) => {   // Si el path es "/yep"
+});*/
+/*app.get('/', (req, res) => {   // Si el path es "/"
     res.sendFile(__dirname + '/public/index.html');
+});*/
+
+app.get('/', (req, res) => {   // Si el path es "/" (renderizada)
+    res.render('home', {       // Pilla el home de views por defecto
+        nombre: 'Pablo Gonzálbez',
+        titulo: 'Título',
+        desc: 'Curso de Node'
+    });
 });
 
-app.get('/elements', (req, res) => {   // Si el path es "/yep"
-    res.sendFile(__dirname + '/public/elements.html');
+app.get('/generic', (req, res) => {   // Si el path es "/generic" (renderizada)
+    res.render('generic', {
+        nombre: 'Pablo Gonzálbez',
+        titulo: 'Título',
+        desc: 'Curso de Node'
+    });
 });
 
-app.get('/generic', (req, res) => {   // Si el path es "/yep"
-    res.sendFile(__dirname + '/public/generic.html');
+app.get('/elements', (req, res) => {   // Si el path es "/elements" (renderizada)
+    res.render('elements', {
+        nombre: 'Pablo Gonzálbez',
+        titulo: 'Título',
+        desc: 'Curso de Node'
+    });
 });
 
 app.get('*', (req, res) => {
     res.sendFile(__dirname + '/PRUEBA/404.html');   // Para mandar un archivo HTML
-                                                           // __dirname pilla el path donde está instalado todo
-                                                           // No funciona el css, creo que es por la web hosteada
+    // __dirname pilla el path donde está instalado todo
+    // No funciona el css, creo que es por la web hosteada
 });
 
 app.listen(port);
